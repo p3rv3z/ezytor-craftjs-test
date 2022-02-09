ed074e84b183e2fec36f7a2969e35369d93a6c37
unknown <nurayni.siddique@gmail.com>
2022-02-09 16:42:55 +0600

    done

--- src/components/rightsidebar/RightSidebar.js
+++ src/components/rightsidebar/RightSidebar.js
@@ -88,6 +88,74 @@ function RightSidebar() {
                                 <input autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="Cover"></input>
                             </div>
                         </div>
+                        {/* Margin*/}
+                        <div className="_add_tab_section_header">
+                            <div className="_add_tab_section_header_icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-opacity="0.7" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="8 12 12 16 16 12"></polyline><line x1="12" y1="8" x2="12" y2="16"></line></svg>
+                            </div>
+                                <h4 className="_add_tab_section_header_title">Margin & Padding</h4>
+                        </div>
+                        <div className="_spacing">
+                            <p class="_spacing_txt">Margin -</p>
+                            <div className="_add_tab_section_size">
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Top:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Bottom:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Left:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Right:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Margin*/}
+                        {/*Padding*/}
+                        <div className="_spacing">
+                            <p class="_spacing_txt">Padding -</p>
+                            <div className="_add_tab_section_size">
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Top:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Bottom:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Left:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                                <div className="_add_tab_section_size_info">
+                                    <div className="_add_tab_section_size_info_txt">
+                                        <h4 className="_add_tab_section_drop_title">Right:</h4>
+                                    </div>
+                                    <input type="number" autocomplete="off" autocorrect="off" autocaptialize="off" spellcheck="false" data-automation-id="css-token-input" placeholder="0px"></input>
+                                </div>
+                            </div>
+                        </div>
+                        {/*Padding*/}
                         <div className="_add_tab_section_header">
                             <div className="_add_tab_section_header_icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-opacity="0.7" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="8 12 12 16 16 12"></polyline><line x1="12" y1="8" x2="12" y2="16"></line></svg>
