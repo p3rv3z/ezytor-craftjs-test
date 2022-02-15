12bacbb5ce15217c2910a54a4fbf62ec4340e1cf
Pervez Ali <contact.pervezali@gmail.com>
2022-02-15 13:10:05 +0600

    movable bug fixed

--- src/components/elements/container/Container.js
+++ src/components/elements/container/Container.js
@@ -6,8 +6,8 @@ export const Container = ({children}) => {
     const { connectors: { connect, drag } } = useNode();
 
     return (
-        <Element is="div" id="container" className="_container_active" ref={ref => connect(drag(ref))} canvas>
+        <div ref={ref => connect(drag(ref))} className="_container_active">
             {children}
-        </Element>
+        </div>
     )
 }
--- src/components/elements/main/Main.js
+++ src/components/elements/main/Main.js
@@ -5,7 +5,7 @@ export const Main = ({children}) => {
     const { connectors: { connect, drag } } = useNode();
 
     return (
-        <div ref={ref => connect(drag(ref))}>
+        <div ref={ref => connect(drag(ref))} className="main">
             {children}
         </div>
     )
